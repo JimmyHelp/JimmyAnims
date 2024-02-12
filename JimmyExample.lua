a0c5aa80fdf5bf1a3af1c87525d959c880a8ca43
JimmyHelp <103872776+JimmyHelp@users.noreply.github.com>
2024-02-12 02:18:49 -0600

    For JimmyAnims V5

--- JimmyExample.lua
+++ JimmyExample.lua
@@ -3,8 +3,7 @@ anims.excluBlendTime = 4
 anims.incluBlendTime = 4
 anims.autoBlend = true
 anims.dismiss = false
-anims.addExcluAnims()
-anims.addIncluAnims()
-anims.addAllAnims()
+anims.addExcluAnimsController()
+anims.addIncluAnimsController()
+anims.addAllAnimsController()
 anims(animations.BBMODEL_NAME_HERE)
---logTable(anims.animsList)
