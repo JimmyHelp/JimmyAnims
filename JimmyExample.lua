952a0ee85935690e3752e7bec50d9482fc500735
JimmyHelp <103872776+JimmyHelp@users.noreply.github.com>
2025-01-15 20:55:51 -0600

    Delete JimmyExample.lua

--- JimmyExample.lua
+++
@@ -1,9 +0,0 @@
-local anims = require('JimmyAnims')
-anims.excluBlendTime = 4
-anims.incluBlendTime = 4
-anims.autoBlend = true
-anims.dismiss = false
-anims.addExcluOverrider()
-anims.addIncluOverrider()
-anims.addAllOverrider()
-anims(animations.BBMODEL_NAME_HERE)
